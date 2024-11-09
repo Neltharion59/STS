cb7ef6db759cdf44c900cc9182c02120b300e69f
Neltharion59 <lukawh5959@gmail.com>
2024-11-09 11:02:22 +0100

    Fix word occurence across paragraphs persistence format

--- Optimizing/vector_modification_scripts/create_word_occurence_across_paragraphs_correct_format_2.py
+++ Optimizing/vector_modification_scripts/create_word_occurence_across_paragraphs_correct_format_2.py
@@ -3,6 +3,11 @@ import os
 with open('../resources/vector/esa_paragraphs_occured_in_counts.txt', 'r', encoding='utf-8') as store_file:
     lines = store_file.readlines()
 
+vector_words = [word for word in get_unique_dataset_words()]
+for word in vector_words:
+    print(word)
+exit(0)
+
 for line in lines:
     print(line)
 exit(0)
