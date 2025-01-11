c4e29969510ea5ec5a93c8641581e8109d3f9a1c
Neltharion59 <lukawh5959@gmail.com>
2025-01-11 14:42:41 +0100

    Fix directory path

--- Optimizing/vector_modification_scripts/create_vectors_fast_text_classic.py
+++ Optimizing/vector_modification_scripts/create_vectors_fast_text_classic.py
@@ -10,7 +10,7 @@ from corpora_modification_scripts.Util import get_unique_dataset_words
 unique_dataset_words = get_unique_dataset_words()
 new_vectors = {}
 
-with open('./resources/vectors/cc.sk.300.vec', 'r', encoding='utf-8', newline='\n', errors='ignore') as vector_file:
+with open('./resources/vector/cc.sk.300.vec', 'r', encoding='utf-8', newline='\n', errors='ignore') as vector_file:
     first = True
     for line in vector_file:
         if first:
