0bbdec6afe101b93967e34cd9569bf34b9d447dd
Neltharion59 <lukawh5959@gmail.com>
2025-01-31 20:02:32 +0100

    check dataset values

--- Optimizing/dataset_modification_scripts/check_dataset_values.py
+++ Optimizing/dataset_modification_scripts/check_dataset_values.py
@@ -4,35 +4,44 @@ import sys
 sys.path.append(root_path)
 
 import math
+from json import loads, dumps
 
 from dataset_modification_scripts.dataset_pool import dataset_pool
+from util.file_handling import write, read
+
+dataset_file_pattern = 'resources/split_datasets/split_dataset_{0}_{1}_sk.json'
 
 for dataset_version in dataset_pool:
     for dataset in dataset_pool[dataset_version]:
-        values = dataset.load_values()
 
-        for method in values:
-            if method == 'gold_standard':
-                continue
+        dataset_file = dataset_file_pattern.format(dataset.name, dataset_version)
+        value_object = loads(read(dataset_file))
+        object_modified = False
 
-            for config in values[method]:
-                vector = [float(x) for x in config['values']]
-
-                issues = False
-                if max(vector) > 1.0:
-                    perc = len([x for x in vector if x > 1.0])/len(vector) * 100
-                    print('MAX Issues with ', method, '. ', perc, '%')
-                    issues = True
-
-                if min(vector) < 0.0:
-                    perc = len([x for x in vector if x < 0.0]) / len(vector) * 100
-                    print('MIN Issues with ', method, '. ', perc, '%')
-                    issues = True
-
-                if len([x for x in vector if math.isnan(x)]) > 0:
-                    perc = len([x for x in vector if math.isnan(x)]) / len(vector) * 100
-                    print('NAN Issues with ', method, '. ', perc, '%')
-                    issues = True
-
-                if issues:
+        for dataset_part in ['Train', 'Validate']:
+            for method in value_object[dataset_part]['features']:
+                if method == 'gold_standard':
                     continue
+
+                for i in range(len(value_object[dataset_part]['features'][method])):
+
+                    vector = value_object[dataset_part]['features'][method][i]['values']
+                    modified = False
+
+                    # Clamp negative values
+                    if min(vector) < 0.0:
+                        vector = [max(0, x) for x in value_object[dataset_part]['features'][method][i]['values']]
+                        modified = True
+
+                    # Normalize
+                    max_value = max(vector)
+                    if max_value > 1.0:
+                        vector = [x/max_value for x in vector]
+                        modified = True
+
+                    if modified:
+                        object_modified = True
+                        value_object[dataset_part]['features'][method][i]['values'] = vector
+
+        if object_modified:
+            write(dataset_file, dumps(value_object, indent=4))
