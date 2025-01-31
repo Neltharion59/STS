0781832599b9c5b8dc8822a1e3d60787a828d18e
Neltharion59 <lukawh5959@gmail.com>
2025-01-31 19:21:11 +0100

    check dataset values

--- Optimizing/dataset_modification_scripts/check_dataset_values.py
+++ Optimizing/dataset_modification_scripts/check_dataset_values.py
@@ -3,12 +3,16 @@ root_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 import sys
 sys.path.append(root_path)
 
-from json import dumps
-
 from dataset_modification_scripts.dataset_pool import dataset_pool
 
 for dataset_version in dataset_pool:
     for dataset in dataset_pool[dataset_version]:
         values = dataset.load_values()
 
-        print(dumps(values, indent=4).split('\n')[:30])
+        for method in values:
+            if method == 'gold_standard':
+                continue
+
+            for config in values[method]:
+                if max([float(x) for x in config['values']]) > 1.0 or min([float(x) for x in config['values']]) < 0.0:
+                    print('Issues with ', method, ' - ', config['args'])
